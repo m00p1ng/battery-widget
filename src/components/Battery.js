3623666ef9f28dc866a098f80c167330311fc44f
mooping <mongkonchai4412@gmail.com>
2018-12-25 14:14:24 +0700

    add more accuracy with charging icon

--- src/components/Battery.js
+++ src/components/Battery.js
@@ -5,18 +5,20 @@ import ChargingBattery from './ChargingBattery'
 
 import '../assets/styles.css'
 
-const renderBattery = (percent, status) => {
-  if (status === 'charging' || status === 'charged') {
+const renderBattery = (percent, status, estimate) => {
+  if (status === 'charged' || status === "AC attached" ||
+    (status === 'charging' && !estimate.includes("0:00 remaining"))
+  ) {
     return <ChargingBattery percentage={percent} />
   }
   return <NormalBattery percentage={percent} />
 }
 
 const Battery = ({ battery }) => {
-  const { percentage, status } = battery
+  const { percentage, status, estimate } = battery
   return (
     <div className="battery-wrapper">
-      {renderBattery(percentage, status)}
+      {renderBattery(percentage, status, estimate)}
       <span id="battery-percent">{percentage}%</span>
     </div>
   )
