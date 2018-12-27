f7fe72e7591134e70f01e916f62d118983c85eca
mooping <mongkonchai4412@gmail.com>
2018-12-27 13:08:05 +0700

    move context menu to a new function

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ const windowHeight = 34
 const windowWidth = 180
 const windowEstimateWidth = 220;
 
+let lastStatus = 'charged'
+let currentPosition = 'Bottom Right'
+let showBatteryEstimate = false
+let showChargeEstimate = false
+let batteryObservable
+
 const createWindow = () => {
   const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize
   app.setName('Battery Widget')
@@ -30,6 +36,73 @@ const createWindow = () => {
 
   mainWindow.on('closed', () => {
     mainWindow = null
+  })
+}
+
+const createContextMenu = () => {
+  const menu = new Menu()
+  const position = ['Top Left', 'Top Right', 'Bottom Left', 'Bottom Right']
+
+  position.forEach((pos) => {
+    menu.append(new MenuItem({
+      label: pos,
+      click: () => {
+        mainWindow.setBounds(getPosition({
+          pos,
+          batteryStatus: lastStatus,
+          showBatteryEstimate,
+          showChargeEstimate,
+        }))
+        currentPosition = pos
+      }
+    }))
+  })
+
+  menu.append(new MenuItem({ type: 'separator' }))
+  menu.append(new MenuItem({
+    label: 'Show Estimate',
+    enabled: false,
+  }))
+  menu.append(new MenuItem({
+    label: 'Battery Time',
+    type: 'checkbox',
+    click: () => {
+      showBatteryEstimate = !showBatteryEstimate
+      mainWindow.webContents.send('show-battery-estimate', showBatteryEstimate)
+      mainWindow.setBounds(getPosition({
+        currentPosition,
+        batteryStatus: lastStatus,
+        showBatteryEstimate,
+        showChargeEstimate
+      }))
+    }
+  }))
+
+  menu.append(new MenuItem({
+    label: 'Charging Time',
+    type: 'checkbox',
+    click: () => {
+      showChargeEstimate = !showChargeEstimate
+      mainWindow.webContents.send('show-charge-estimate', showChargeEstimate)
+      mainWindow.setBounds(getPosition({
+        currentPosition,
+        batteryStatus: lastStatus,
+        showBatteryEstimate,
+        showChargeEstimate,
+      }))
+    }
+  }))
+
+  menu.append(new MenuItem({ type: 'separator' }))
+  menu.append(new MenuItem({
+    label: 'Quit',
+    click: () => {
+      app.quit()
+    }
+  }))
+
+  mainWindow.webContents.on('context-menu', (event) => {
+    menu.popup()
   })
 }
 
@@ -76,12 +149,6 @@ const getPosition = ({ position, batteryStatus, showBatteryEstimate, showChargeE
 }
 
 app.on('ready', () => {
-  let lastStatus = 'charged'
-  let currentPosition = 'Bottom Right'
-
-  let showBatteryEstimate = false
-  let showChargeEstimate = false
-
   createWindow()
   const URL = (process.env.NODE_ENV !== 'development') ?
     `file://${path.join(__dirname, './build/index.html')}` :
@@ -90,8 +157,13 @@ app.on('ready', () => {
   mainWindow.loadURL(URL)
 
   mainWindow.webContents.on('did-finish-load', () => {
-    BatteryLevel().subscribe(result => {
-      mainWindow.webContents.send('battery-level', result)
+    createContextMenu()
+    app.dock.hide()
+
+    batteryObservable = BatteryLevel().subscribe(result => {
+      if ('webContents' in mainWindow) {
+        mainWindow.webContents.send('battery-level', result)
+      }
       const { status } = result
 
       if (status !== lastStatus) {
@@ -104,77 +176,11 @@ app.on('ready', () => {
       }
       lastStatus = status
     })
-    app.dock.hide()
-  })
-
-  const menu = new Menu()
-  const position = ['Top Left', 'Top Right', 'Bottom Left', 'Bottom Right']
-
-  position.forEach((pos) => {
-    menu.append(new MenuItem({
-      label: pos,
-      click: () => {
-        mainWindow.setBounds(getPosition({
-          pos,
-          batteryStatus: lastStatus,
-          showBatteryEstimate,
-          showChargeEstimate,
-        }))
-        currentPosition = pos
-      }
-    }))
-  })
-
-  mainWindow.webContents.on('did-finish-load', () => {
-    menu.append(new MenuItem({ type: 'separator' }))
-    menu.append(new MenuItem({
-      label: 'Show Estimate',
-      enabled: false,
-    }))
-    menu.append(new MenuItem({
-      label: 'Battery Time',
-      type: 'checkbox',
-      click: () => {
-        showBatteryEstimate = !showBatteryEstimate
-        mainWindow.webContents.send('show-battery-estimate', showBatteryEstimate)
-        mainWindow.setBounds(getPosition({
-          currentPosition,
-          batteryStatus: lastStatus,
-          showBatteryEstimate,
-          showChargeEstimate
-        }))
-      }
-    }))
-    menu.append(new MenuItem({
-      label: 'Charging Time',
-      type: 'checkbox',
-      click: () => {
-        showChargeEstimate = !showChargeEstimate
-        mainWindow.webContents.send('show-charge-estimate', showChargeEstimate)
-        mainWindow.setBounds(getPosition({
-          currentPosition,
-          batteryStatus: lastStatus,
-          showBatteryEstimate,
-          showChargeEstimate,
-        }))
-      }
-    }))
-
-    menu.append(new MenuItem({ type: 'separator' }))
-    menu.append(new MenuItem({
-      label: 'Quit',
-      click: () => {
-        app.quit()
-      }
-    }))
-  })
-
-  mainWindow.webContents.on('context-menu', (event) => {
-    menu.popup()
   })
 })
 
 app.on('window-all-closed', function () {
+  batteryObservable.unsubscribe()
   app.quit()
 })
 
