01654606a1e0c5b0a4212492fe0a3f53c42bdf8e
Mongkonchai Priyachiwa <mongkonchai4412@gmail.com>
2019-02-24 15:58:10 +0700

    use react hooks for timer

--- src/components/Timer/TimerClock.js
+++ src/components/Timer/TimerClock.js
@@ -1,71 +1,106 @@
-import React, { Component } from 'react'
+import React, { useReducer, useRef } from 'react'
 import parseMs from 'parse-ms'
 
 import ControlButton from './ControlButton'
 
-class TimerClock extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      time: 0,
-      isOn: false,
-      start: 0
-    }
-    this.startTimer = this.startTimer.bind(this)
-    this.pauseTimer = this.pauseTimer.bind(this)
-    this.stopTimer = this.stopTimer.bind(this)
+const START_TIMER = 'START_TIMER'
+const STOP_TIMER = 'STOP_TIMER'
+const PAUSE_TIMER = 'PAUSE_TIMER'
+const SET_TIME = 'SET_TIME'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case START_TIMER:
+      return {
+        isOn: true,
+        time: state.time,
+        start: Date.now() - state.time
+      }
+    case SET_TIME:
+      return {
+        ...state,
+        time: Date.now() - state.start
+      }
+    case PAUSE_TIMER:
+      return {
+        ...state,
+        isOn: false,
+      }
+    case STOP_TIMER:
+      return {
+        ...state,
+        time: 0,
+        isOn: false,
+      }
+    default:
+      return state
+  }
+}
+
+const useTimer = () => {
+  const initialState = {
+    time: 0,
+    start: 0,
+    isOn: false,
   }
 
-  startTimer() {
-    this.setState({
-      isOn: true,
-      time: this.state.time,
-      start: Date.now() - this.state.time
-    })
-    this.timer = setInterval(() => {
-      this.setState({
-        time: Date.now() - this.state.start
-      })
-    }, 1);
+  const [{ time, isOn }, dispatch] = useReducer(reducer, initialState)
+
+  const intervalRef = useRef()
+  const startTimer = () => {
+    dispatch({ type: START_TIMER })
+    intervalRef.current = setInterval(() => dispatch({ type: SET_TIME }), 0)
+  }
+  const pauseTimer = () => {
+    dispatch({ type: PAUSE_TIMER })
+    clearInterval(intervalRef.current)
+  }
+  const stopTimer = () => {
+    dispatch({ type: STOP_TIMER })
+    clearInterval(intervalRef.current)
   }
 
-  pauseTimer() {
-    this.setState({ isOn: false })
-    clearInterval(this.timer)
+  return {
+    time,
+    isOn,
+    startTimer,
+    pauseTimer,
+    stopTimer,
+  }
+}
+
+const formatOutput = (time) => {
+  const zeroPad = (num) => num < 10 ? `0${num}` : num
+  const newTime = parseMs(time)
+
+  let output = `${zeroPad(newTime.minutes)}:${zeroPad(newTime.seconds)}`
+  if (newTime.hours > 0) {
+    output = `${newTime.hours}:${output}`
   }
 
-  stopTimer() {
-    this.setState({ time: 0, isOn: false })
-  }
+  return output
+}
 
-  formatOutput() {
-    const zeroPad = (num) => num < 10 ? `0${num}` : num
-    const time = parseMs(this.state.time)
+const TimerClock = () => {
+  const {
+    time, isOn,
+    startTimer, pauseTimer, stopTimer
+  } = useTimer()
 
-    let output = `${zeroPad(time.minutes)}:${zeroPad(time.seconds)}`
-    if (time.hours > 0) {
-      output = `${time.hours}:${output}`
-    }
-
-    return output
-  }
-
-  render() {
-    return (
-      <div id="timer-wrapper">
-        <div id="display-time">
-          {this.formatOutput()}
-          <ControlButton
-            startTimer={this.startTimer}
-            pauseTimer={this.pauseTimer}
-            stopTimer={this.stopTimer}
-            time={this.state.time}
-            isOn={this.state.isOn}
-          />
-        </div>
+  return (
+    <div id="timer-wrapper">
+      <div id="display-time">
+        {formatOutput(time)}
+        <ControlButton
+          startTimer={startTimer}
+          pauseTimer={pauseTimer}
+          stopTimer={stopTimer}
+          time={time}
+          isOn={isOn}
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default TimerClock
